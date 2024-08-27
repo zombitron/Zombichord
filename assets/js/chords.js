310778f352f31983e21934cc3bf379e4d3c7a82e
Clara Rigaud <clararigaud0@gmail.com>
2024-08-27 14:36:39 +0200

    petit ajout socket pour mes tests

--- assets/js/chords.js
+++ assets/js/chords.js
@@ -1,3 +1,5 @@
+import '/socket.io/socket.io.js';
+var socket = io();
 var chordNamesMaj = ["A", "E", "B", "F#", "Db", "Ab", "Eb", "Bb", "F", "C", "G", "D"];
 var chordNamesm = ["F#m", "C#m", "G#m", "Ebm", "Bbm", "Fm", "Cm", "Gm", "Dm", "Am", "Em", "Bm"];
 
@@ -10,16 +12,17 @@ function createButtons(circleClass, chordNames, radius) {
 
         button.classList.add("chordButton");
         button.textContent = chordNames[i];
-
+        button.value = chordNames[i];
         var angle = (i / chordNames.length) * 360;
         var x = radius * Math.cos(angle * (Math.PI / 180));
         var y = radius * Math.sin(angle * (Math.PI / 180));
 
-
         button.style.left = `calc(50% + ${x}px )`;
         button.style.top = `calc(50% + ${y}px )`;
 
-        // TODO : Ajouter le listener pour declencher l'envoie du message WSocket
+        button.addEventListener("click", function(){
+            socket.emit("message", {id: "chord", value: this.value});
+        })
         circle.appendChild(button);
     }
 }
@@ -29,8 +32,4 @@ createButtons('.outerCircle', chordNamesMaj, 180);
 
 document.addEventListener("click", (event) => {
     document.querySelector('body').requestFullscreen();
-    document.addEventListener("dblclick", (ev) => {
-        window.location.reload();
-        document.querySelector('body').requestFullscreen();
-    });
 }, { once: true });
