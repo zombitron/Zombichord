5e07991b59e5f338124003296495a7384c87f89d
Marion Jolas <marion.jolas@noesya.coop>
2024-08-28 15:27:16 +0200

    keys

--- assets/js/strumplate.js
+++ assets/js/strumplate.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("strumPlate");
+var ctx = canvas.getContext("2d");
 
 function getMousePosition(canvas, event) {
     var rect = canvas.getBoundingClientRect();
@@ -10,6 +11,23 @@ function getMousePosition(canvas, event) {
 canvas.addEventListener("touchmove", function (e) {
     getMousePosition(canvas, e);
 });
+
+function createKeys() {
+    var keyWidth = canvas.width / 18;
+    for (var i = 0; i < 18; i++) {
+        ctx.fillStyle = "#66828E";
+        ctx.fillRect(i * keyWidth, 0, keyWidth, canvas.height);
+
+        ctx.strokeStyle = "#fff";
+        ctx.strokeRect(i * keyWidth, 0, keyWidth, canvas.height);
+    }
+}
+
+createKeys();
+
+
+
+
 
 document.addEventListener("click", function(event) {
     document.querySelector('body').requestFullscreen();
