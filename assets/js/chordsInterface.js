30e191279ee174fe68159e92e546d1ca11f059a4
Clara Rigaud <clararigaud0@gmail.com>
2024-09-04 16:46:54 +0200

    chordCircle size

--- assets/js/chordsInterface.js
+++ assets/js/chordsInterface.js
@@ -1,46 +1,48 @@
 import '/socket.io/socket.io.js';
 import chordsCircle from '/assets/js/zombichord/ui/chordsCircle.js'; // charge la vue circle
 
-// listen ton chords event and send socket
-var socket = io();
-socket.emit('message', { id: 'hello'});
-
-var chordsContainer = document.querySelector(".chordsContainer");
-
-chordsContainer.classList.add('loading');
-chordsCircle.initialize(chordsContainer);
-
-chordsCircle.element.addEventListener('chordTrigger', function (e) {
-    socket.emit('message', { id: 'chordTrigger', value: e.detail });
-});
-
-chordsCircle.element.addEventListener('chordRelease', function (e) {
-    socket.emit('message', { id: 'chordRelease', value: e.detail });
-});
-
-chordsCircle.element.addEventListener('7th', function (e) {
-    socket.emit('message', { id: '7th', value: e.detail });
-});
-
-chordsCircle.element.addEventListener('chordMemory', function (e) {
-    socket.emit('message', { id: 'chordMemory', value: e.detail });
-});
-
-socket.on('state', function (state) {
-    if (state == 'loaded') {
-        console.log("loaded")
-    }
-
-    if (state == 'started') {
-        chordsContainer.classList.remove('loading');
-        console.log("stared")
-    }
-
-    if (state == 'loading') {
-        chordsContainer.classList.add('loading');
-        console.log("loading")
-    }
-});
+window.addEventListener('load', function(){
+    // listen ton chords event and send socket
+    var socket = io();
+    socket.emit('message', { id: 'hello'});
+    
+    var chordsContainer = document.querySelector(".chordsContainer");
+    
+    chordsContainer.classList.add('loading');
+    chordsCircle.initialize(chordsContainer);
+    
+    chordsCircle.element.addEventListener('chordTrigger', function (e) {
+        socket.emit('message', { id: 'chordTrigger', value: e.detail });
+    });
+    
+    chordsCircle.element.addEventListener('chordRelease', function (e) {
+        socket.emit('message', { id: 'chordRelease', value: e.detail });
+    });
+    
+    chordsCircle.element.addEventListener('7th', function (e) {
+        socket.emit('message', { id: '7th', value: e.detail });
+    });
+    
+    chordsCircle.element.addEventListener('chordMemory', function (e) {
+        socket.emit('message', { id: 'chordMemory', value: e.detail });
+    });
+    
+    socket.on('state', function (state) {
+        if (state == 'loaded') {
+            console.log("loaded")
+        }
+    
+        if (state == 'started') {
+            chordsContainer.classList.remove('loading');
+            console.log("stared")
+        }
+    
+        if (state == 'loading') {
+            chordsContainer.classList.add('loading');
+            console.log("loading")
+        }
+    });
+})
 
 document.addEventListener("click", function (event) {
     event.preventDefault;
--- assets/js/zombichord/ui/chordsCircle.js
+++ assets/js/zombichord/ui/chordsCircle.js
@@ -5,8 +5,8 @@ var chordsCircle = {
     currentChord: null,
     initialize: function (container) {
         this.element = container;
-        this.createCircle('.innerCircle', this.chordsMin, 120);
-        this.createCircle('.outerCircle', this.chordsMaj, 180);
+        this.createCircle('.innerCircle', this.chordsMin);
+        this.createCircle('.outerCircle', this.chordsMaj);
 
         // cree le 7th
         this.createOptionButton('.septiemeButton', '7th');
@@ -15,12 +15,12 @@ var chordsCircle = {
 
         this.currentChord = this.element.querySelector(".currentChord");
     },
-    createCircle: function (circleClass, chordNames, radius) {
+    createCircle: function (circleClass, chordNames) {
         var circle = this.element.querySelector(circleClass);
         for (let i = 0; i < chordNames.length; i++) {
             var angle = (i / chordNames.length) * 360;
-            var x = radius * Math.cos(angle * (Math.PI / 180));
-            var y = radius * Math.sin(angle * (Math.PI / 180));
+            var x = 50 * Math.cos(angle * (Math.PI / 180));
+            var y = 50 * Math.sin(angle * (Math.PI / 180));
             var button = this.createButton(x, y, chordNames[i]);
             circle.appendChild(button);
         }
@@ -30,8 +30,8 @@ var chordsCircle = {
         button.classList.add("chordButton");
         button.textContent = chord;
         button.value = chord;
-        button.style.left = `calc(50% + ${posX}px )`;
-        button.style.top = `calc(50% + ${posY}px )`;
+        button.style.left = `calc(50% + ${posX}% )`;
+        button.style.top = `calc(50% + ${posY}% )`;
 
         button.addEventListener("touchstart", function (e) {
             e.preventDefault();
